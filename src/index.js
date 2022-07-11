709eebdddc13c8a60257f0135c0a905bfdd0fe6c
Arch1198 <archana.gangurde@torinit.ca>
2022-07-11 17:14:22 +0530

    first task commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,48 @@
+
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+import ReactDOM from 'react-dom';
+import './index.css'
+ReactDOM.render(
+    <>
+    <header class="head">
+      <h1>E-Shopping</h1>
+      <ul class="nav">
+      <li class="active">Home</li>
+      <li>Search</li>
+      <li>Shop</li>
+      <li>About</li>
+      </ul>
+    <button>Sign In</button>
+    </header>
+    <section class="sectionCards">
+    
+    <div class="cat">
+    <h1>Fasion</h1>
+    <div class="cardContainer">
+    <div class="card">
+      <img src="./images/image1.png" alt=""/>
+      <p>Blue Printed</p>
+      <h3>₹494 <strike>₹1,481</strike> 67% off</h3>
+    </div>
+    <div class="card">
+      <img src="./images/image2.png" alt=""/>
+      <p>white gown Party Dress</p>
+      <h3>₹4940 <strike>₹11,499</strike> 67% off</h3>
+    </div>
+    
+    <div class="card">
+      <img src="./images/image3.jpg" alt=""/>
+      <p> Bridal Dress</p>
+      <h3>₹11,000 <strike>₹19,000</strike> 67% off</h3>
+    </div>
+    
+    </div>
+    </div>
+    
+    
+    
+    </section>
+    </>
+    ,document.getElementById('root'))
+    
